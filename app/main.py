9c5ce4ff38d1a711986b2c03562ba02adbe20863
franco-kwaba <franco@kwaba.com.br>
2025-06-17 09:41:03 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,19 +1,23 @@
-#from app.db import criar_banco
-#from app.modelos import adicionar_conselheiro, adicionar_queixa, adicionar_avaliacao, exibir_ranking
-
-# Cria o banco na primeira execução
-#criar_banco("data/condominio.db")
-
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+import os
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "API FastAPI funcionando no Render!"}
+# Ajuda a montar caminhos a partir do app/
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-@app.get("/hello")
-def hello():
-    return {"greet": "Olá!"}
+# Templates
+TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
+templates = Jinja2Templates(directory=TEMPLATES_DIR)
+
+# Arquivos estáticos (se precisar)
+STATIC_DIR = os.path.join(BASE_DIR, "web", "static")
+app.mount("/static", StaticFiles(directory=STATIC_DIR), name="static")
 
 
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
